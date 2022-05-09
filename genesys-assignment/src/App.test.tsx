f804eeb8c6bc5619be56ef8884aaa67b59d53125
Chris Nurrenberg <cdnurrenberg@gmail.com>
2022-05-09 13:56:44 -0400

    final CRA cleanup touches?

--- genesys-assignment/src/App.test.tsx
+++ genesys-assignment/src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* 
+ I did not get to writing tests for this project. Similar to the thinking I outlined
+ in JOURNAL.md, this would absolutely have to be something that gets considered and
+ completed to company standards before publishing a pull request or approving / merging one.
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ For component-based tests, I like doing DOM snapshots so it's explicitly clear when a style changes
+ From PR to PR.
+
+ it would look something like
+
+ it('renders a story card properly', () => {
+   act(() => {
+     render(<StoryComponent {...props}, parent) 
+   });
+   expect(component.innerHTML).toMatchSnapshot();
+ })
+
+ So every time a component is modified we will have to update snapshots and confirm that
+ the test results are still in alignment with expectations, design, etc.
+
+ I will admit I need more experience API testing. I have worked along side tools that test
+ endpoints and simulate endpoints for other testing, but I have not done much of this myself
+ unfortunately.
+*/
