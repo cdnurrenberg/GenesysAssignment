9f29af3b179787e4c35712b2a0d93d6f12dfc4d5
Chris Nurrenberg <cdnurrenberg@gmail.com>
2022-05-08 21:57:13 -0400

    add footer

--- genesys-assignment/src/App.tsx
+++ genesys-assignment/src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/components.css'
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 const App = () => {
-
   return (
     <div className="App">
       <Header />
-      {/* <NavBar? */}
       <StoryFeed />
+      <Footer />
     </div>
   );
 }
